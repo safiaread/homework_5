2824c572cf4a9cc3487ee995f2e2cd9cb3193d20
Safia Read <safia.read@emory.edu>
2024-04-28 23:58:33 -0400

    push push

--- submission_2/analysis/analysis_sub2.r
+++ submission_2/analysis/analysis_sub2.r
@@ -3,16 +3,21 @@ library(acs)
 
 if (!require("pacman")) install.packages("pacman")
 pacman::p_load(tidyverse, ggplot2, dplyr, lubridate, readr, readxl, scales, acs, tidyr)
+#Take Puerto Rico and D.c. out
 
 #Question 1
 #Plot the share of the adult population with direct purchase health insurance over time.
 data <- read.delim("/Users/safiaread/Desktop/homework_5/data/output/acs_medicaid.txt")
 head(data)
-data%>%
+data <- data%>%
+subset(State != "District of Columbia" & State != "Puerto Rico")
+
+figure_1 <- data%>%
 group_by(year)%>%
 summarise(adult_share = sum(ins_direct)/sum(adult_pop))%>%
 ggplot(aes(y = adult_share, x = year))+
-geom_line()
+geom_line()+
+labs(x = "Year", y = "% Direct Purchase Health Insurance", title = "Share of the Adult Population with Direct Purchase Health Insurance over Time")
 
 #Question 2
 #Discuss the reduction in direct purchase health insurance in later years. Can you list a couple of policies that might have affected the 
@@ -20,37 +25,39 @@ geom_line()
 
 #Question 3
 #Plot the share of the adult population with Medicaid over time.
-data%>%
+figure_3 <- data%>%
 group_by(year)%>%
 summarise(adult_share = sum(ins_medicaid)/sum(adult_pop))%>%
 ggplot(aes(y = adult_share, x = year))+
-geom_line()
+geom_line()+
+labs(x = "Year", y = "% Medicaid Insurance", title = "Share of the Adult Population with Medicaid Insurance over Time")
 
 #Question 4
 #Plot the share of uninsured over time, separately by states 
 #that expanded Medicaid in 2014 versus those that did not. 
 #Drop all states that expanded after 2014.
-data <- data%>%
+data_q4 <- data%>%
 mutate(expand_14 = ifelse(expand_year != "2014" | is.na(expand_year), FALSE, TRUE))
 
 table(data$expand_14)
 
-data%>%
+figure_4 <- data_q4%>%
 filter(is.na(expand_year)| expand_14 == TRUE)%>%
 group_by(year, expand_14)%>%
 summarise(adult_share = sum(uninsured)/sum(adult_pop))%>%
 ggplot(aes(y = adult_share, x = year, color = expand_14))+
-geom_line()
+geom_line()+
+labs(x = "Year", y = "% Uninsured", title = "Share of the Adult Population that is Uninsured", color = "Expanded Medicaid in 2014")
 
 
 #Question 5
 #Calculate the average percent of uninsured individuals in 2012 and 2015, 
 #separately for expansion and non-expansion states. 
 #Present your results in a basic 2x2 DD table.
-data <- data%>%
+data_q5 <- data%>%
 mutate(expand_14 = ifelse(expand_year != "2014" | is.na(expand_year), "not expanded", "expanded"))
 
-data%>%
+figure_5 <- data_q5%>%
 filter(year == "2012" | year == "2015")%>%
 group_by(year, expand_14)%>%
 summarise(percent_uninsured = mean(uninsured/adult_pop, na.rm = T))%>%
@@ -61,14 +68,14 @@ pivot_wider(names_from = expand_14, values_from = percent_uninsured)
 #states that expanded in 2014 versus those that never expanded.
 library(modelsummary)
 
-reg.data <- data %>% filter(expand_year==2014 | is.na(expand_year), !is.na(expand_ever)) %>%
+data_q6 <- data %>% filter(expand_year==2014 | is.na(expand_year), !is.na(expand_ever)) %>%
   mutate(perc_unins=uninsured/adult_pop,
          post = (year>=2014), 
          treat=post*expand_ever)
 
-summary(lm(perc_unins ~ post + expand_ever + post*expand_ever, data=reg.data))
-dd.ins.reg <- lm(perc_unins ~ post + expand_ever + post*expand_ever, data=reg.data)
-modelsummary(list("DD (2014)"=dd.ins.reg),
+#summary(lm(perc_unins ~ post + expand_ever + post*expand_ever, data=reg.data))
+reg_q6 <- lm(perc_unins ~ post + expand_ever + post*expand_ever, data=data_q6)
+modelsummary(list("DD (2014)"=reg_q6),
              shape=term + statistic ~ model, 
              gof_map=NA,
              coef_omit='Intercept',
@@ -84,11 +91,11 @@ modelsummary(list("DD (2014)"=dd.ins.reg),
 
 library(fixest)
 
-m.dd <- lm(perc_unins ~ post + expand_ever + treat, data=reg.data)
+m.dd.7 <- lm(perc_unins ~ post + expand_ever + treat, data=data_q6)
 
-m.twfe <- feols(perc_unins ~ treat | State + year, data=reg.data)
+m.twfe.7 <- feols(perc_unins ~ treat | State + year, data=data_q6)
 
-msummary(list("DD"=m.dd, "TWFE"=m.twfe),
+modelsummary(list("DD"=m.dd.7, "TWFE"=m.twfe.7),
          shape=term + statistic ~ model, 
          gof_map=NA,
          coef_omit='Intercept',
@@ -98,15 +105,25 @@ msummary(list("DD"=m.dd, "TWFE"=m.twfe),
 #Question 8
 #Repeat the analysis in question 7 but include all states 
 #(even those that expanded after 2014). Are your results different? If so, why?
-reg.data <- data %>% filter(!is.na(expand_ever)) %>%
+data_q8 <- data %>% filter(!is.na(expand_ever)) %>%
   mutate(perc_unins=uninsured/adult_pop,
          post = (year>=2014), 
          treat=post*expand_ever)
+         
+data_q8_2 <- data %>% 
+  mutate(perc_unins=uninsured/adult_pop,
+         post = (year>=2014),
+         treat=case_when(
+    year>=expand_year & !is.na(expand_year) ~ 1,
+    is.na(expand_year) ~ 0,
+    year<expand_year & !is.na(expand_year) ~ 0)
+  )
 
-m.dd <- lm(perc_unins ~ post + expand_ever + treat, data=reg.data)
-m.twfe <- feols(perc_unins ~ treat | State + year, data=reg.data)
+m.dd.8 <- lm(perc_unins ~ post + expand_ever + treat, data=data_q8)
+m.twfe.8 <- feols(perc_unins ~ treat | State + year, data=data_q8)
+fe.est2.8 <- feols(perc_unins ~ treat | State + year, data=data_q8_2) 
 
-msummary(list("DD"=m.dd, "TWFE"=m.twfe),
+modelsummary(list("DD"=m.dd.8, "TWFE"=m.twfe.8, "Time Variance" = fe.est2.8),
          shape=term + statistic ~ model, 
          gof_map=NA,
          coef_omit='Intercept',
@@ -117,17 +134,17 @@ msummary(list("DD"=m.dd, "TWFE"=m.twfe),
 #Question 9
 #Provide an “event study” graph showing the effects of Medicaid expansion in each year. Use the specification that includes 
 #state and year fixed effects, limited to states that expanded in 2014 or never expanded.
-reg.dat <- data %>% 
+data_q9 <- data %>% 
   filter(expand_year==2014 | is.na(expand_year), !is.na(expand_ever)) %>%
   mutate(perc_unins=uninsured/adult_pop,
          post = (year>=2014), 
          treat=post*expand_ever)
 
-mod.twfe <- feols(perc_unins~i(year, expand_ever, ref=2013) | State + year,
+mod.twfe.9 <- feols(perc_unins~i(year, expand_ever, ref=2013) | State + year,
                   cluster=~State,
-                  data=reg.dat)
+                  data=data_q9)
 
-iplot(mod.twfe, 
+iplot(mod.twfe.9, 
       xlab = 'Time to treatment',
       main = 'Event study')
 
@@ -135,18 +152,23 @@ iplot(mod.twfe,
 #Repeat part 9 but again include states that expanded after 2014. Note: this is tricky…you need to put all states onto 
 #“event time” to create this graph.
 
-reg.dat <- data %>% 
+data_q10 <- data %>% 
   filter(!is.na(expand_ever)) %>%
   mutate(perc_unins=uninsured/adult_pop,
          post = (year>=2014), 
          treat=post*expand_ever,
          time_to_treat = ifelse(expand_ever==FALSE, 0, year-expand_year),
-         time_to_treat = ifelse(time_to_treat < -3, -3, time_to_treat))
+         time_to_treat = ifelse(time_to_treat < -4, -4, time_to_treat))
 
-mod.twfe <- feols(perc_unins~i(time_to_treat, expand_ever, ref=-1) | State + year,
+mod.twfe.10 <- feols(perc_unins~i(time_to_treat, expand_ever, ref=-1) | State + year,
                   cluster=~State,
-                  data=reg.dat)
+                  data=data_q10)
 
-iplot(mod.twfe, 
+iplot(mod.twfe.10, 
       xlab = 'Time to treatment',
       main = 'Event study')
+
+#Check this not exactly
+
+rm(list=c("data", "data_q4", "data_q5", "data_q9", "data_q10")) # nolint
+save.image("submission_2/Hwk5_workspace.Rdata")
